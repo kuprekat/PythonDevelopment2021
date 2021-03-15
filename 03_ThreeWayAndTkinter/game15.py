57649cdd8180c255cd73fb1d51bae5731571ebcc
katya <kuprekat@yandex.ru>
2021-03-15 16:11:16 +0300

    new_game button added

--- 03_ThreeWayAndTkinter/game15.py
+++ 03_ThreeWayAndTkinter/game15.py
@@ -6,8 +6,15 @@ class Application(tk.Tk):
         self.CreateWidgets()
 
     def CreateWidgets(self):
+        for i in range(0, 5):
+            self.rowconfigure(i, weight=1)
+        for i in range(0, 4):
+            self.columnconfigure(i, weight=1)
+        self.newgame_button = tk.Button(self, text='New Game', command=self.NewGame)
+        self.newgame_button.grid(column = 0, row = 0, columnspan = 4, sticky = "WE")
         self.quit_button = tk.Button(self, text='Quit', command=self.quit)
         self.quit_button.grid(column = 3, row = 5, columnspan = 2)
+
     
     def NewGame(self):
         pass
