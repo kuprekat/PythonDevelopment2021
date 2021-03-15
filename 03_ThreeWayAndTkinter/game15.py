81af2d669359aa1e662fad36c21b5ffdcfa44117
katya <kuprekat@yandex.ru>
2021-03-15 16:54:30 +0300

    random numbers generation

--- 03_ThreeWayAndTkinter/game15.py
+++ 03_ThreeWayAndTkinter/game15.py
@@ -1,4 +1,12 @@
 import tkinter as tk
+import random
+
+def generate_numbers():
+    seq_1_to_15 = list(range(1, 16))
+    random.shuffle(seq_1_to_15)
+    print(seq_1_to_15)
+    return seq_1_to_15
+
 
 class Application(tk.Tk):
     def __init__(self, master=None):
@@ -11,7 +19,7 @@ class Application(tk.Tk):
         for i in range(0, 4):
             self.columnconfigure(i, weight=1)
         self.newgame_button = tk.Button(self, text='New Game', command=self.NewGame)
-        self.newgame_button.grid(column = 0, row = 0, columnspan = 4, sticky = "WE")
+        self.newgame_button.grid(column = 0, row = 0, columnspan = 4, sticky = "NEW")
         self.quit_button = tk.Button(self, text='Quit', command=self.quit)
         self.quit_button.grid(column = 3, row = 5, columnspan = 2)
 
@@ -19,10 +27,16 @@ class Application(tk.Tk):
 
     
     def NewGame(self):
+        numbers = generate_numbers()
         for i in range(0, 4):
             for j in range(0, 4):
-                self.squares[i][j].config(text = "LOL")
-                self.squares[i][j].grid(column = i, row = j + 1)
+                if i == 3 and j == 3:
+                    self.squares[i][j].grid_forget()
+                    break
+                self.squares[i][j].config(text = numbers[i * 4 + j])
+                self.squares[i][j].grid(column = i, row = j + 1, sticky = "NEWS")
+
+        
 
 app = Application()
 app.title('Play 15')
