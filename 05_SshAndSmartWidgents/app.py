0a2f7f65d47a7e99e754210fc0c3c6c4b75a3a38
katya <kuprekat@yandex.ru>
2021-03-28 22:13:39 +0300

    Initial: main window creation

---
+++ 05_SshAndSmartWidgents/app.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter.font import Font as tkFont
+
+class Application(tk.Frame):
+    '''Sample tkinter application class'''
+
+    def __init__(self, master=None, title="<application>", **kwargs):
+        '''Create root window with frame, tune weight and resize'''
+        super().__init__(master, **kwargs)
+        self.master.title(title)
+        self.master.geometry("480x360")
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.grid(sticky="NEWS")
+        self.create_widgets()
+        for column in range(self.grid_size()[0]):
+            self.columnconfigure(column, weight=1)
+        for row in range(self.grid_size()[1]):
+            self.rowconfigure(row, weight=1)
+
+    def create_widgets(self):
+        '''Create all the widgets'''
+
+    def bind_widgets(self):
+        '''Bind events to the widgets'''
+
+class App(Application):
+    pass
+
+
+app = App(title="Editor")
+app.mainloop()
