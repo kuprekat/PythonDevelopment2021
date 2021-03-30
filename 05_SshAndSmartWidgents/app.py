23f541ddd71a1e10100874b9b8a149771e6fbd8b
katya <kuprekat@yandex.ru>
2021-03-30 21:40:58 +0300

    string output added

--- 05_SshAndSmartWidgents/app.py
+++ 05_SshAndSmartWidgents/app.py
@@ -2,11 +2,35 @@ import tkinter as tk
 from tkinter.font import Font as tkFont
 import random as random
 
-colors = ["dark orange", "DarkOliveGreen3", "SkyBlue2", "lemon chiffon", "MistyRose2", "gold", "khaki", "SlateGray3"]
+colors = ["255.140.0", "162.205.90", "126.192.238", "255.250.205", "238.213.210", "255.215.0", "240.230.140", "159.182.205"]
+# colors = ["dark orange", "DarkOliveGreen3", "SkyBlue2", "lemon chiffon", "MistyRose2", "gold", "khaki", "SlateGray3"]
 
 def rand_col():
     r = random.randint(0, len(colors) - 1)
     return colors[r]
+
+def get_color(str):
+    nums = list(map(int, str.split('.')))
+    print(nums)
+    res = "#" + format(nums[0], '0>2X') + format(nums[1], '0>2X') + format(nums[2], '0>2X')
+    print(res)
+    return res
+
+class Oval():
+    def __init__(self,coords, color):
+        self.coords1 = coords
+        self.coords2 = None
+        self.color = color
+        self.outline = "#000000"
+        self.width = 1
+        self.id = id
+        self.object = None
+        self.string = None
+
+    def get_string(self, final_coords):
+        self.string = f"oval at {self.coords1} - {final_coords}, color: {self.color}, outline color: {self.outline}, width: {self.width}"
+        return self.string
+
 
 class Application(tk.Frame):
     '''Sample tkinter application class'''
@@ -34,8 +58,10 @@ class App(Application):
         super().__init__(**kwargs)
 
         self.ovals = []
+        self.cur_id = 0
         self.oval = None
         self.color = "white"
+        self.creating_oval = False
 
     def create_widgets(self):
         self.frame1 = tk.LabelFrame(self)
@@ -68,20 +94,27 @@ class App(Application):
     def button_press(self, event):
         # print("Button pressed")
         self.coords = (event.x, event.y)
-        self.ovals.append(self.coords)
-        self.color = rand_col()
+        self.color = get_color(rand_col())
 
     def mouse_move(self, event):
         # print(("moving"))
+        if not self.creating_oval:
+            self.creating_oval = True
+            self.cur_id = len(self.ovals)
+            self.ovals.append(Oval(self.coords, self.color))
         if self.oval:
             self.canv.delete(self.oval)
-        self.oval = self.canv.create_oval(*self.coords, event.x, event.y, tags="obj", fill=self.color, outline = "black", width=1)
+        self.oval = self.canv.create_oval(*self.coords, event.x, event.y, tags="obj", fill=self.color, outline = "#000000", width=1)
 
     def button_release(self, event):
+        self.ovals[self.cur_id].object = self.oval
         self.coords = (None, None)
         self.oval = None
-
-
+        if self.creating_oval:
+            string = self.ovals[self.cur_id].get_string((event.x, event.y))
+            self.txt.insert('end', string + "\n")
+        self.creating_oval = False
+        
 
 
 app = App(title="Editor")
